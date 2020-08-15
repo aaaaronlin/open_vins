a4cf0473a1e5253750718831691f3181f78afa18
aaron <aaronlin@berkeley.edu>
2020-08-15 12:53:12 -0700

    final commit, buildable

--- ov_msckf/src/utils/parse_json.h
+++ ov_msckf/src/utils/parse_json.h
@@ -5,7 +5,7 @@
 #ifndef OPEN_VINS_PARSE_JSON_H
 #define OPEN_VINS_PARSE_JSON_H
 
-#include <json.hpp>
+#include <utils/json.hpp>
 #include <core/VioManagerOptions.h>
 
 namespace ov_msckf {
@@ -18,12 +18,12 @@ namespace ov_msckf {
         params.record_timing_filepath = "/home/aaron/Desktop/openvins_data/traj_timing.txt";
 
         // filter parameters
-        params.state_options.max_clones = 11;
-        params.state_options.max_slam = 50;
+        params.state_options.max_clone_size = 11;
+        params.state_options.max_slam_features = 50;
         params.state_options.max_slam_in_update = 25;
         params.state_options.max_msckf_in_update = 999;
-        params.state_options.feat_rep_msckf = "GLOBAL_3D";
-        params.state_options.feat_rep_slam = "GLOBAL_3D";
+        params.state_options.feat_rep_msckf = LandmarkRepresentation::from_string("GLOBAL_3D");
+        params.state_options.feat_rep_slam = LandmarkRepresentation::from_string("GLOBAL_3D");
         // Global gravity
         std::vector<double> gravity = {0.0, 0.0, 9.81};
         params.gravity << gravity.at(0),gravity.at(1),gravity.at(2);
@@ -33,7 +33,7 @@ namespace ov_msckf {
         params.fast_threshold = 15;
         params.grid_x = 4;
         params.grid_y = 3;
-        params.min_px_dst = 10;
+        params.min_px_dist = 10;
         params.num_pts = 400;
 
         // IMU Parameters
@@ -41,7 +41,7 @@ namespace ov_msckf {
         params.init_window_time = 0.5;
 
         params.msckf_options.sigma_pix = 1;
-        params.msckf_options.chi2_multiplier = 1;
+        params.msckf_options.chi2_multipler = 1;
         // noise density
         params.imu_noises.sigma_w = 0.007855;
         params.imu_noises.sigma_a = 0.004334;
@@ -56,7 +56,7 @@ namespace ov_msckf {
         // Camera Parameters
         // TODO add multiple cameras
         bool is_fisheye = false;
-        std::vector<int> matrix_wd = {640,480};
+        std::vector<int> matrix_wh = {640,480};
         std::pair<int,int> wh(matrix_wh.at(0),matrix_wh.at(1));
         // intrinsics
         Eigen::Matrix<double,8,1> cam_calib;
