91ed90f292f6aabf838fdc6fca7e9a5ad2706747
Aaron Lin <aaronlin@berkeley.edu>
2020-09-02 21:00:02 -0700

    header

--- ov_msckf/src/pc_msckf.h
+++ ov_msckf/src/pc_msckf.h
@@ -19,58 +19,35 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#ifndef OV_MSCKF_PC_MSCKF_H
+#define OV_MSCKF_PC_MSCKF_H
 
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
 
 #include "core/VioManager.h"
 #include "core/VioManagerOptions.h"
 #include "utils/parse_json.h"
 
-using namespace ov_msckf;
+namespace ov_msckf {
 
-VioManager* sys;
+	class PCMSCKF {
+	public:
+		PCMSCKF();
+		/* move state forward with imu measurement */
+		void predict_imu(const std::vector<float>& gyro, const std::vector<float>& acc, const float t);
 
-double time_buffer = -1;
-cv::Mat img0_buffer, img1_buffer;
+		/* move state forward with one camera measurement */
+		void update_monocular(const cv::Mat& img0, const float t);
 
-void predict_imu(const std::vector<float>& gyro, const std::vector<float>& acc, const float t) {
-    Eigen::Vector3d wm, am;
-    wm << gyro[0], gyro[1], gyro[2];
-    am << acc[0], acc[1], acc[2];
+		/* move state forward with two camera measurements */
+		void update_stereo(const cv::Mat& img0, const cv::Mat& img1, const float t0, const float t1);
 
-    sys->feed_measurement_imu(t, wm, am);
+	protected:
+		VioManager* sys;
+
+		double time_buffer;
+		cv::Mat img0_buffer, img1_buffer;
+
+	};
 }
 
-void update_monocular(const cv::Mat& img0, const float t) {
-    // fill buffer
-    if(img0_buffer.rows == 0) {
-        time_buffer = t;
-        img0_buffer = img0.clone();
-        return;
-    }
-
-    sys->feed_measurement_monocular(time_buffer, img0_buffer, 0);
-
-    time_buffer = t;
-    img0_buffer = img0.clone();
-}
-
-void update_stereo(const cv::Mat& img0, const cv::Mat& img1, const float t0, const float t1) {
-    // fill buffer
-    if(img0_buffer.rows == 0 || img1_buffer.rows == 0) {
-        time_buffer = t0;
-        img0_buffer = img0.clone();
-        time_buffer = t1;
-        img1_buffer = img1.clone();
-        return;
-    }
-
-    sys->feed_measurement_stereo(time_buffer, img0_buffer, img1_buffer, 0, 1);
-
-    time_buffer = t0;
-    img0_buffer = img0.clone();
-    time_buffer = t1;
-    img1_buffer = img1.clone();
-}
+#endif
